592c64dd4224e32052f9e4e35cf3c955e8837b5d
atsushi0124 <h19170004@gmail.com>
2024-02-29 11:58:54 +0900

    idに修正

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let video = document.getElementById("my-video");
 let text = document.getElementById("text");
 
 video.addEventListener("loadeddata", function () {
-  const load = document.getElementsByClassName("load");
+  const load = document.getElementsById("load");
   load.style.display = "none";
   console.log("loadeddata発生");
 });
